e51553d0baff37339047450a69db3e70377af961
sonofman60 <benjamin.davis@admsystemsllc.com>
2021-12-28 14:20:28 -0500

    Install the pg library

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { Pool } = require('pg');
+const conn = new Pool ( {connectionString: process.env.DATABASE_URL});
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -6,4 +8,19 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => listUsers(req, res))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+  async function listUsers(req, res) {
+    try {
+    const db = await conn.connect()
+    const result = await db.query('SELECT * FROM users');
+    const results = { users: (result) ? result.rows : null};
+    res.render('pages/index', results );
+    db.release();
+    } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+    }
+    }
